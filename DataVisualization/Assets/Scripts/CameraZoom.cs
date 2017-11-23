97f2dc1d2f6ba524ef7e117e955442652033593b
HokutoTateyama <tateyama.hokuto@fixer.co.jp>
2017-11-24 02:33:05 +0900

    カメラのズーム機能

---
+++ DataVisualization/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.position += transform.forward * scroll * 8.0f;
+    }
+}
